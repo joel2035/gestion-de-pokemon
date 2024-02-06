b2367fbdc06829a0cce2c428a7dfc7da572c7fb5
Joel <joel.pokam@hetic.net>
2024-02-06 13:59:05 +0100

    fix detail-pokemon

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import POKEMONS from "../../models/mock-pokemon";
+import Pokemon from "../../models/pokemon";
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -6,6 +9,16 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DetailPokemonComponent {
-
+export class DetailPokemonComponent implements OnInit{
+    pokemonList:Pokemon[];
+    pokemon:Pokemon | undefined;
+  constructor(private route:ActivatedRoute) {
+  }
+ ngOnInit() {
+      this.pokemonList = POKEMONS
+    const pokemonId:string|null = this.route.snapshot.paramMap.get('id')
+     if(pokemonId){
+         this.pokemon = this.pokemonList.find(pokemon=>pokemon.id == +pokemonId )
+     }
+ }
 }
